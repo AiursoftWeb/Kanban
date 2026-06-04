268f5b78e0fe2d02a5f31789a94e91699bcb8cde
Aimer Neige <aimer@linux.com>
2026-06-04 15:42:37 +0000

    fix: update name to Aiursoft Kanban

--- src/Aiursoft.Kanban/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
+++ src/Aiursoft.Kanban/Views/Shared/Components/MarketingNavbar/MarketingNavbarViewModel.cs
@@ -2,6 +2,6 @@ namespace Aiursoft.Kanban.Views.Shared.Components.MarketingNavbar;
 
 public class MarketingNavbarViewModel
 {
-    public string ProjectName { get; set; } = "Aiursoft Template";
+    public string ProjectName { get; set; } = "Aiursoft Kanban";
     public string LogoUrl { get; set; } = "/logo.svg";
 }
