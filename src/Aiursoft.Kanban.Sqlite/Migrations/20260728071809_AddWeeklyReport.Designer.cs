51519049e87e1d49248acaf7d86a5eb47850f09f
zetian <zetian@aiursoft.com>
2026-07-28 15:23:31 +0800

    migrate databases

--- src/Aiursoft.Kanban.Sqlite/Migrations/20260728071809_AddWeeklyReport.Designer.cs
+++ src/Aiursoft.Kanban.Sqlite/Migrations/20260728071809_AddWeeklyReport.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Aiursoft.Kanban.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aiursoft.Kanban.Sqlite.Migrations
 {
     [DbContext(typeof(SqliteContext))]
-    partial class SqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20260728071809_AddWeeklyReport")]
+    partial class AddWeeklyReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
